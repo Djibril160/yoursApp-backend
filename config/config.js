171e9bc2289aa6aa6a4d36b4c46a4a8d7a81800a
djibril soukouna <djib_cisse_9@hotmail.com>
2023-11-22 23:26:40 +0100

    configuration done

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
 
 mongoose.connect(process.env.CONNECTION_MDB, { connectTimeoutMS: 2000 })
-.then(() => console.log('=> Bien joué mon pote („• ᴗ •„)'))
+.then(() => console.log('=> Well connected dude („• ᴗ •„)'))
 .catch(error => console.error(error))
